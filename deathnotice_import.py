d7ad988bc18b5d6f657a5a7f2f205691bcdb3e98
Eric Bower <neurosnap@gmail.com>
2015-10-22 17:58:18 -0400

    better separation

--- deathnotice_import.py
+++ deathnotice_import.py
@@ -21,103 +21,9 @@ import pymysql
 import xml.etree.ElementTree as ET
 
 from strip_html import strip_tags
+from obit import Obit
 
 __version__ = '0.0.1'
-
-def sorted_nicely(_list):
-    """ Sort the given iterable in the way that humans expect."""
-    convert = lambda text: int(text) if text.isdigit() else text
-    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
-    return sorted(_list, key = alphanum_key)
-
-class Obit(object):
-    """ Data container for each obituary in the XML file. """
-    def __init__(self, first_name, last_name, ad_number,  publication,
-            text, images, siicode, created_at='', subclass_code='', record_id=None):
-        self.record_id = record_id
-        self.first_name = first_name
-        self.last_name = last_name
-        self.full_name = ', '.join([last_name, first_name])
-        self.ad_number = ad_number
-        self.publication = publication
-        self.text = text.encode('utf-8')
-        self.images = images
-        self.siicode = siicode
-        self.subclass_code = subclass_code
-        if not created_at:
-            created_at = date.today()
-        self.created_at = created_at
-
-        self.inserted = False
-        self.updated = False
-
-    def __str__(self):
-        return "<Obit Name: {}\nAd#: {}\nPost Date: {}\nImages: {}\nSiicode: {}>" \
-            .format(self.full_name, self.ad_number, self.created_at, self.images, self.siicode)
-
-    @property
-    def image_str(self):
-        return ';'.join(self.images)
-
-    @property
-    def sql_params(self):
-        return [self.full_name, self.text, str(self.created_at), self.image_str, self.siicode, self.ad_number, self.subclass_code, self.first_name, self.last_name, self.publication]
-
-    def find(self, connection):
-        """ Find the record in our database """
-        with connection.cursor() as cursor:
-            sql = "SELECT `recordID` FROM `death_notices` WHERE `adnum`=%s AND `bdate`=%s"
-            cursor.execute(sql, (self.ad_number, self.created_at))
-            result = cursor.fetchone()
-
-            if result is None:
-                return None
-
-            self.record_id = result['recordID']
-            return self.record_id
-
-    def save(self, connection):
-        self.find(connection)
-
-        if self.record_id is not None:
-            self.update(connection)
-        else:
-            self.insert(connection)
-
-    def insert(self, connection):
-        """ Insert new obituary into database """
-
-        with connection.cursor() as cursor:
-            sql = """INSERT INTO `death_notices`
-            (`fname`, `btext`, `bdate`, `image`, `siicode`, `adnum`, `subclass`,
-             `firstname`, `lastname`, `publication`)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-            cursor.execute(sql, self.sql_params)
-
-        connection.commit()
-
-        self.inserted = True
-
-    def update(self, connection, record_id=None):
-        """ Update an already existing obit in database """
-        if record_id is None and self.record_id is None:
-            raise Exception("To update an obit, you must supply its corresponding recordID")
-        if record_id is None:
-            record_id = self.record_id
-
-        with connection.cursor() as cursor:
-            sql = """UPDATE `death_notices`
-            SET `fname`=%s, `btext`=%s, `bdate`=%s, `image`=%s, `siicode`=%s,
-                `adnum`=%s, `subclass`=%s, `firstname`=%s, `lastname`=%s,
-                `publication`=%s
-            WHERE recordID=%s"""
-            params = self.sql_params
-            params.append(record_id)
-            cursor.execute(sql, params)
-
-        connection.commit()
-
-        self.updated = True
 
 def parse_obits(fname, date=None):
     tree = ET.parse(fname)
@@ -163,6 +69,13 @@ def parse_obits(fname, date=None):
         images = sorted_nicely(final_images)
 
         yield Obit(first_name, last_name, ad_number, publication, text, images, siicode, date, subclass_code)
+
+def sorted_nicely(_list):
+    """ Sort the given iterable in the way that humans expect."""
+    convert = lambda text: int(text) if text.isdigit() else text
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
+    return sorted(_list, key = alphanum_key)
+
 
 if __name__ == '__main__':
     _date = date.today()
---
+++ obit.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+class Obit(object):
+    """ Data container for each obituary in the XML file. """
+    def __init__(self, first_name, last_name, ad_number,  publication,
+            text, images, siicode, created_at='', subclass_code='', record_id=None):
+        self.record_id = record_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = ', '.join([last_name, first_name])
+        self.ad_number = ad_number
+        self.publication = publication
+        self.text = text.encode('utf-8')
+        self.images = images
+        self.siicode = siicode
+        self.subclass_code = subclass_code
+        if not created_at:
+            created_at = date.today()
+        self.created_at = created_at
+
+        self.inserted = False
+        self.updated = False
+
+    def __str__(self):
+        return "<Obit Name: {}\nAd#: {}\nPost Date: {}\nImages: {}\nSiicode: {}>" \
+            .format(self.full_name, self.ad_number, self.created_at, self.images, self.siicode)
+
+    @property
+    def image_str(self):
+        return ';'.join(self.images)
+
+    @property
+    def sql_params(self):
+        return [
+            self.full_name, self.text, str(self.created_at), self.image_str,
+            self.siicode, self.ad_number, self.subclass_code, self.first_name,
+            self.last_name, self.publication,
+        ]
+
+    def find(self, connection):
+        """ Find the record in our database """
+        with connection.cursor() as cursor:
+            sql = "SELECT `recordID` FROM `death_notices` WHERE `adnum`=%s AND `bdate`=%s"
+            cursor.execute(sql, (self.ad_number, self.created_at))
+            result = cursor.fetchone()
+
+            if result is None:
+                return None
+
+            self.record_id = result['recordID']
+            return self.record_id
+
+    def save(self, connection):
+        self.find(connection)
+
+        if self.record_id is not None:
+            self.update(connection)
+        else:
+            self.insert(connection)
+
+    def insert(self, connection):
+        """ Insert new obituary into database """
+
+        with connection.cursor() as cursor:
+            sql = """INSERT INTO `death_notices`
+            (`fname`, `btext`, `bdate`, `image`, `siicode`, `adnum`, `subclass`,
+             `firstname`, `lastname`, `publication`)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+            cursor.execute(sql, self.sql_params)
+
+        connection.commit()
+
+        self.inserted = True
+
+    def update(self, connection, record_id=None):
+        """ Update an already existing obit in database """
+        if record_id is None and self.record_id is None:
+            raise Exception("To update an obit, you must supply its corresponding recordID")
+        if record_id is None:
+            record_id = self.record_id
+
+        with connection.cursor() as cursor:
+            sql = """UPDATE `death_notices`
+            SET `fname`=%s, `btext`=%s, `bdate`=%s, `image`=%s, `siicode`=%s,
+                `adnum`=%s, `subclass`=%s, `firstname`=%s, `lastname`=%s,
+                `publication`=%s
+            WHERE recordID=%s"""
+            params = self.sql_params
+            params.append(record_id)
+            cursor.execute(sql, params)
+
+        connection.commit()
+
+        self.updated = True
+
