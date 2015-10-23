190c6714d1ee7956bbbf0f515a110306a61de68d
Mike Varano <mikevee313@gmail.com>
2015-10-23 13:08:38 -0400

    added all emails

--- deathnotice_importer/send_email.py
+++ deathnotice_importer/send_email.py
@@ -7,7 +7,7 @@ def send_email(msg):
     now = datetime.now()
     msg = MIMEText(msg)
     msg["From"] = "deathnotices@michigan.com"
-    msg["To"] = 'mvarano@michigan.com' #', '.join(['neurosnap@gmail.com', 'mvarano@michigan.com', 'rwilliams@michigan.com'])
+    msg["To"] = ', '.join(['neurosnap@gmail.com', 'mvarano@michigan.com', 'rwilliams@michigan.com'])
     msg["Subject"] = "Death Notice Feed for {}-{}-{}".format(now.year, now.month, now.day)
     try:
         p = Popen(["/usr/sbin/sendmail", "-t"], stdin=PIPE)
