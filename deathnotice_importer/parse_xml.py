00110a100ac6005b7fb939bf6d1cd346c74d331e
Eric Bower <neurosnap@gmail.com>
2015-10-29 16:55:03 -0400

    quickfix

--- deathnotice_importer/parse_xml.py
+++ deathnotice_importer/parse_xml.py
@@ -49,7 +49,7 @@ def get_obit(pub_code, icons=[], date=None):
         final_images.append(image)
 
     images = sorted_nicely(final_images)
-    return Obit(first_name, last_name, ad_number, publication, text, images, siicode, date, subclass_code, use_s3=os.getenv('USE_S3', False))
+    return Obit(first_name, last_name, ad_number, publication, text, images, siicode, date, subclass_code, use_s3=os.getenv('USE_S3', 0))
 
 def sorted_nicely(_list):
     """ Sort the given iterable in the way that humans expect."""
