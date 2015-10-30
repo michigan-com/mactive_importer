098b507d4716958f9cfef53ff4134df59137efd5
Eric Bower <neurosnap@gmail.com>
2015-10-30 11:53:22 -0400

    updated bat file

--- run.bat
+++ run.bat
@@ -4,5 +4,6 @@ setx DN_PASS ""
 setx AWS_ACCESS_KEY_ID ""
 setx AWS_SECRET_ACCESS_KEY ""
 
-cd "C:\Program Files\feeds\deathnotices\deathnotice_importer"
-python -m deathnotice_importer --s3 -f "C:\Program Files\feeds\deathnotices\feeds\deathnotices.txt" -d mideathnotices/assets/images/dnimages %*
+cd "mactive_importer"
+python -m mactive_importer.deathnotices --s3 -f "..\deathnotices_rs\deathnotices.xml" -d mideathnotices/assets/images/dnimages %*
+
