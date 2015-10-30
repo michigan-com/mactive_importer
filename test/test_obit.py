2bfea1c68bcc7dda9416743abc70a3bf43a692b1
Mike Varano <mikevee313@gmail.com>
2015-10-30 10:38:15 -0400

    removed test/data from dependencies

--- test/test_obit.py
+++ test/test_obit.py
@@ -4,18 +4,8 @@ import os
 import shutil
 
 from deathnotice_importer.obit import Obit
-from .lib import src_dir, dest_dir, get_test_images
 
 class TestObit(unittest.TestCase):
-    def tearDown(self):
-        # clear out images after every test method
-        for fname in os.listdir(dest_dir):
-            file_path = os.path.join(dest_dir, fname)
-            if os.path.isfile(file_path):
-                os.remove(file_path)
-            elif os.path.isdir(file_path):
-                shutil.rmtree(file_path)
-
     def get_default_args(self):
         return {
             'first_name': 'test name',
@@ -54,45 +44,3 @@ class TestObit(unittest.TestCase):
         args['images'] = ['img1.jpg', 'img2.png']
         obit = Obit(**args)
         self.check_images(obit, args['images'])
-
-
-    def test_successful_copy(self):
-        args = self.get_default_args()
-        args['images'] = get_test_images()
-        obit = Obit(**args)
-        self.check_images(obit, args['images'])
-
-        # Copy and verify image copying
-        obit.copy_images(src_dir, dest_dir)
-        for img in obit.images:
-            if not os.path.exists(os.path.join(dest_dir, img)):
-                self.fail('image {} didn\'t copy to {}'.format(img, dest_dir))
-
-    def test_copy_images_dont_exist(self):
-        args = self.get_default_args()
-        args['images'] = ['asdfasdfasdfasdfasdf.jpg', 'asdfasdfasdf.png',
-               'thisisatestname.jpg', 'thisisalsoatest.png']
-        obit = Obit(**args)
-        self.check_images(obit, args['images'])
-
-        obit.copy_images(src_dir, dest_dir)
-        self.assertEqual(len(os.listdir(dest_dir)), 0, 'No images should have been copied to ./data/dest_dir')
-
-    def test_copy_images_inavlid_dest_dir(self):
-        args = self.get_default_args()
-        args['images'] = get_test_images()[:10]
-        obit = Obit(**args)
-        self.check_images(obit, args['images'])
-
-        obit.copy_images(src_dir, '/this/destination/doesn\'t exist')
-        self.assertEqual(len(os.listdir(dest_dir)), 0, 'No images should have been copied to ./data/dest_dir')
-
-    def test_copy_images_invalid_src_dir(self):
-        args = self.get_default_args()
-        args['images'] = get_test_images()[:10]
-        obit = Obit(**args)
-        self.check_images(obit, args['images'])
-
-        obit.copy_images('/this/destination/doesn\'t exist', dest_dir)
-        self.assertEqual(len(os.listdir(dest_dir)), 0, 'No images should have been copied to ./data/dest_dir')
-
