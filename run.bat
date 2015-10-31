907709686391b76fc935bb9354e8d6fbcd8e7a9e
Eric Bower <neurosnap@gmail.com>
2015-10-31 12:15:05 -0400

    batch file fix

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ setx DN_PASS ""
 setx AWS_ACCESS_KEY_ID ""
 setx AWS_SECRET_ACCESS_KEY ""
 
-cd "mactive_importer"
+cd /d K:\Mactive\Web\nm_feeds\mactive_importer
 python -m mactive_importer.deathnotices --s3 -f "..\deathnotices_rs\deathnotices.xml" -d mideathnotices/assets/images/dnimages %*
 
