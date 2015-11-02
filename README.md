fbd93a658154ea513a2447746c1334fe5005a1d9
Eric Bower <neurosnap@gmail.com>
2015-11-02 11:40:14 -0500

    loggly logger update

--- README.md
+++ README.md
@@ -6,6 +6,16 @@ Importer for all things Mactive at michigan.com (which is too many things)
 mkvirtualenv mactive
 pip install -r requirements.txt
 ```
+Environment Variables
+---------------------
+
+* DN\_HOST : mySQL IP, default: 'localhost'
+* DN\_DB : mySQL database name, default: 'death\_notices'
+* DN\_USER : mySQL user name, default: ''
+* DN\_PASS : mySQL user password, default: ''
+* AWS\_ACCESS\_KEY\_ID : AWS S3 access key
+* AWS\_SECRET\_ACCESS\_KEY : AWS Secret access key
+* LOGGLY : Loggly API KEY when presetn will send logs to loggly
 
 # Test
 nosetest -s
--- mactive_importer/log.py
+++ mactive_importer/log.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 import os
 import logging
-import loggly.handlers
 
-PACKAGE_DIR = os.path.dirname(__file__)
+#from loggly.handlers import HTTPSHandler
 
-logging.config.fileConfig(os.path.join(PACKAGE_DIR, 'loggly.conf'))
-logger = logging.getLogger('deathnotices')
+_LOGGLY_KEY = os.getenv('LOGGLY', False)
+
+_fmt = "(%(name)s) %(filename)s:%(lineno)d: %(message)s"
+logging.basicConfig(format=_fmt)
+logger = logging.getLogger("deathnotices")
+logger.setLevel(logging.INFO)
+
+if _LOGGLY_KEY:
+    _url = "/inputs/{}/tag/deathnotices".format(_LOGGLY_KEY)
+    _loggly_handler = logging.handlers.HTTPHandler("logs-01.loggly.com", _url, method="POST", secure=True)
+    logger.addHandler(_loggly_handler)
+
+logger.info("THIS IS A TEST")
+logger.info("CMON BABY!")
