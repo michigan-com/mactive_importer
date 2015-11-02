dedfaaca59162c80ebfd81a61f91dcca0e9fac6a
Eric Bower <neurosnap@gmail.com>
2015-11-02 14:55:00 -0500

    loggly??"

--- mactive_importer/log.py
+++ mactive_importer/log.py
@@ -8,11 +8,12 @@ _LOGGLY_KEY = os.getenv('LOGGLY', False)
 
 _fmt = "(%(name)s) %(filename)s:%(lineno)d: %(message)s"
 logging.basicConfig(format=_fmt)
-logger = logging.getLogger("deathnotices")
+logger = logging.getLogger("mactive_importer")
 logger.setLevel(logging.INFO)
 
 if _LOGGLY_KEY:
-    _url = "/inputs/{}/tag/deathnotices".format(_LOGGLY_KEY)
+    print("FOUND LOGGLY KEY, PIPING LOGS TO LOGGLY")
+    _url = "/inputs/{}/tag/mactive_importer".format(_LOGGLY_KEY)
     _loggly_handler = logging.handlers.HTTPHandler("logs-01.loggly.com", _url, method="POST", secure=True)
     logger.addHandler(_loggly_handler)
 
